d09f0cc125b20c9b3bb380b6047530f68e36da1f
Fathima Shameema <fathimashameema116@gmail.com>
2025-11-26 18:53:50 +0530

    commit after reset

---
+++ lib/widgets/container_field.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ContainerField extends StatelessWidget {
+  const ContainerField({
+    super.key,
+    required this.child,
+    this.height,
+    this.onTap,
+  });
+
+  final Widget child;
+  final double? height;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        alignment: Alignment.center,
+        width: double.infinity,
+        height: height ?? 50,
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          border: Border.all(color: Colors.black87),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: child,
+      ),
+    );
+  }
+}
